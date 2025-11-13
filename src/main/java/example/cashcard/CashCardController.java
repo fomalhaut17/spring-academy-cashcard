3a45ca04afe54d3c6fd82753b82e0f8130ea07c6
leewonseok <fomalhaut17@gmail.com>
2025-11-13 23:19:39 +0900

    add CashCardController with basic findById endpoint and integration test

---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,16 @@
+package example.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+class CashCardController {
+    @GetMapping("/{requestedId}")
+    private ResponseEntity<CashCard> findById() {
+        CashCard cashCard = new CashCard(99L, 123.45);
+        return ResponseEntity.ok(cashCard);
+    }
+}
