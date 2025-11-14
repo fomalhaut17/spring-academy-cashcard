2ab4a75cb7b1df316414552bf5035c71954b57c6
leewonseok <fomalhaut17@gmail.com>
2025-11-14 22:53:06 +0900

    add CashCard list serialization and deserialization tests with sample JSON data

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -8,13 +8,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.web.servlet.LocaleResolver;
 
 import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.annotation.DirtiesContext.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 class CashCardApplicationTests {
     @Autowired
     TestRestTemplate restTemplate;
